38f3d09fa42b1ebc6deaed130956edfadcfce829
mktakev <38002608+mktakev@users.noreply.github.com>
2018-04-02 13:32:09 +0300

    Add files via upload

--- Problem_1.R
+++ Problem_1.R
@@ -1,1 +1,25 @@
 
+# a)
+x <- c(4, 1, 1, 4)
+
+# b)
+y <- c(1, 4)
+
+# c)
+setdiff(x,y)
+
+# d)
+s <- c(x, y)
+
+#e)
+rep(c(4, 1, 1, 4, 1, 4), times = 10)
+length(c) 
+
+#f)
+rep(s, each = 3)
+
+#g)
+seq(7, 21)
+
+#h)
+length(seq)
--- Problem_2.R
+++ Problem_2.R
@@ -1,1 +1,46 @@
 
+# a)
+xmin <- c(23.0, 20.5, 28.2, 20.3, 22.4, 17.2, 18.2)
+xmax <- c(25.0, 22.8, 31.2, 27.3, 28.4, 20.2, 24.1)
+
+# b)
+xmax- xmin
+
+# c)
+mean(xmin)
+mean(xmax)
+
+# d)
+xmin[xmin<mean(xmin)]
+
+# e)
+xmax[xmax>mean(xmax)]
+
+# f)
+setNames(xmin, c("03Mon18", "04Tue18", "05Wed18", "04Thu18", "05Fri18", "06Sat18", "07Sun18"))
+setNames(xmax, c("03Mon18", "04Tue18", "05Wed18", "04Thu18", "05Fri18", "06Sat18", "07Sun18"))
+        
+# g)
+temperatures <- data.frame(
+  df.xmin = xmin,
+  df.xmax = xmax)
+
+# h)
+temperatures$xminFahrenheit <- 9/5*xmin+32
+
+# i)
+Fahrenheit <- data.frame(df.xminFahrenheit = xminFahrenheit)
+
+# j-i)
+monthFahrenheit <- data.frame(
+  df.xminFahrenheit = xminFahrenheit [1:5])
+
+# j-ii)
+monthFh <- data.frame(
+  df.xminFahrenheit = xminFahrenheit[c(-6, -7)])
+
+
+
+
+
+  
